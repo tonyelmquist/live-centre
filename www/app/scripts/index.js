20b9d4b988df1af9ad0816e9973b94f22bd202d0
John Anton "Tony" Elmquist <tel@futureuniverse.com>
2017-04-04 09:26:39 +0200

    Added loglevel dependency

--- www/app/scripts/index.js
+++ www/app/scripts/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './components/app';
+import log from './middleware/logger'
 
 render( <App /> , document.getElementById('root'));
