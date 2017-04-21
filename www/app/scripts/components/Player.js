6449bb5a5581c389061ad1d99b53d1aa1bd2fa10
John Anton "Tony" Elmquist <tel@futureuniverse.com>
2017-04-21 14:39:49 +0200

    fixed autoplay and control params

--- www/app/scripts/components/Player.js
+++ www/app/scripts/components/Player.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
- 
-
-
 
 // https://download.mediabankweb.com/online/72ad0542c6144c978dbb9154fc606291/58f9ef3a/THEFUTUREG/201703/954844/954844_proxy.mov
 
 export default class Player extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {url: ''};
-  }
-
   render () {
-    return <ReactPlayer playing='false' controls='true' url='https://download.mediabankweb.com/online/72ad0542c6144c978dbb9154fc606291/58f9ef3a/THEFUTUREG/201703/954844/954844_proxy.mov' playing />
+    return <ReactPlayer 
+      playing={false}
+      controls={true} 
+      url='https://download.mediabankweb.com/online/72ad0542c6144c978dbb9154fc606291/58f9ef3a/THEFUTUREG/201703/954844/954844_proxy.mov' 
+      />
   }
 }
