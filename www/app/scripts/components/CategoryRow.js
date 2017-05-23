6955971a7904ad0dc1b9efe05b7d4e2236ef8db0
John Anton "Tony" Elmquist <tel@futureuniverse.com>
2017-05-23 09:30:54 +0200

    LIVE-32 slideys with buttons

--- www/app/scripts/components/CategoryRow.js
+++ www/app/scripts/components/CategoryRow.js
@@ -1,60 +1,101 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Item from '../components/Item';
-import {Row} from 'react-flexbox-grid';
 import Slider from 'react-slick';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import {grey800} from 'material-ui/styles/colors';
 
 class CategoryRow extends Component {
+
+    constructor(props) {
+        super(props)
+        this.next = this
+            .next
+            .bind(this)
+        this.previous = this
+            .previous
+            .bind(this)
+    }
+    next() {
+        this
+            .slider
+            .slickNext()
+    }
+    previous() {
+        this
+            .slider
+            .slickPrev()
+    }
+
     render() {
-            var settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      }, {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }]
-    };
+        var settings = {
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                }, {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                }, {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
 
         if (this.props.category) {
             var videos = this
                 .props
                 .videos
                 .map(function (video, i) {
-                    return (<div><Item key={i} video={video}/></div>);
+                    return (
+                        <div><Item key={i} video={video}/></div>
+                    );
                 });
 
         } else {
             var videos = '';
         }
 
-        return (<div>
-                <h1 className='rowTitle'>{this.props.category}</h1>
-                <Slider {...settings}>
-                    {videos}
-                </Slider>
-            </div>);
+        return (
+            <div>
+                <div className="rowHeader">
+                    <h3 className='rowTitle'>{this.props.category}</h3>
+                    <div className='sliderButtons'>
+                        <IconButton onTouchTap={this.previous}>
+                            <FontIcon className="material-icons" color={grey800}>arrow_back</FontIcon>
+                        </IconButton>
+                        <IconButton onTouchTap={this.next}>
+                            <FontIcon className="material-icons" color={grey800}>arrow_forward</FontIcon>
+                        </IconButton>
+                    </div>
+                </div>
+                <div className="slider">
+                    <Slider ref={c => this.slider = c} {...settings}>
+                        {videos}
+                    </Slider>
+                </div>
+            </div>
+        );
     }
 }
 
--- www/app/scripts/components/Item.js
+++ www/app/scripts/components/Item.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import { Col } from 'react-flexbox-grid';
-import { GridTile} from 'material-ui/GridList';
+import {Col} from 'react-flexbox-grid';
+import {GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import PlayCircleOutline from 'material-ui/svg-icons/av/play-circle-outline';
 import {videoSelected, invalidateSelected} from '../actions/video';
@@ -14,19 +14,25 @@ class Item extends Component {
             .props
             .dispatch(videoSelected(`https://www.mediabank.me/download/manifest.php?assetid=${assetid}`));
     }
-	render () {
+    render() {
 
-		return (
-                <div className="videoThumb">
-                    <GridTile
-                        title={this.props.video.title}
-                        subtitle={<span>by <b>{this.props.video.author}</b></span>}
-                        actionIcon={<IconButton onTouchTap={() => {this.handlePlay(this.props.video.assetid);} }><PlayCircleOutline color="white" /></IconButton>}>
-                        <img src={this.props.video.thumbnail} />
-                    </GridTile>
-                </div>
-		);
-	}
+        return (
+            <div className="videoThumb">
+                <GridTile
+                    title={this.props.video.title}
+                    subtitle={< span > by < b > {
+                    this.props.video.author
+                } </b></span >}
+                    actionIcon={< IconButton onTouchTap = {
+                    () => {
+                        this.handlePlay(this.props.video.assetid);
+                    }
+                } > <PlayCircleOutline color="white"/> </IconButton>}>
+                    <img src={this.props.video.thumbnail}/>
+                </GridTile>
+            </div>
+        );
+    }
 };
 
 const mapStateToProps = (state) => {
