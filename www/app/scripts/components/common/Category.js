5db2f4d5a6d70d9c5d65f4dade2102134bd2727b
Akmal Fayziyev <afs@futureuniverse.com>
2017-05-31 13:17:33 +0200

    LIVE-33 Modified page styles

--- www/app/scripts/components/common/Category.js
+++ www/app/scripts/components/common/Category.js
@@ -2,34 +2,30 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {NavLink, Link} from 'react-router-dom';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import Video from '../../components/Item';
+import VideoItem from '../../components/Item';
 import IconButton from 'material-ui/IconButton';
 import BackButton from 'material-ui/svg-icons/hardware/keyboard-backspace';
 import {fullWhite, blueGrey900} from 'material-ui/styles/colors';
 
 const styles = {
     mediumIcon: {
-        width: 36,
-        height: 36
+        width: 32,
+        height: 32
     },
     medium: {
         width: 64,
         height: 64,
-        padding: 10
+        padding: 18
     }
 };
 
-const iconStyle = {
-    width: 36,
-    height: 36
-};
 
 class Category extends Component {
     _renderVideos = () =>
         this.props.videos.map((video)=>{
             return (
-                <Col xs={6} md={3}>
-                    <Video video={video}/>
+                <Col xs={6} md={2}>
+                    <VideoItem video={video}/>
                 </Col>
             );
 
