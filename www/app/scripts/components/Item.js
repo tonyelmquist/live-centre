c7cad39071f2ae4fdc3ff600536bc4332d92e40c
Akmal Fayziyev <afs@futureuniverse.com>
2017-06-06 14:15:01 +0200

    LIVE-36 Added additional icon on video item to open product hover. Added some styling to homepage icons

--- www/app/scripts/components/Item.js
+++ www/app/scripts/components/Item.js
@@ -5,7 +5,21 @@ import {Col} from 'react-flexbox-grid';
 import {GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import PlayCircleOutline from 'material-ui/svg-icons/av/play-circle-outline';
+import Info from 'material-ui/svg-icons/action/info';
+import {amber300, yellow500, blue500, fullWhite } from 'material-ui/styles/colors';
 import {videoSelected, invalidateSelected} from '../actions/video';
+
+const styles = {
+    mediumIcon: {
+        width: 36,
+        height: 36
+    },
+    medium: {
+        width: 64,
+        height: 64,
+        padding: 8
+    }
+};
 
 class Item extends Component {
     _handlePlay = (assetid) => {
@@ -19,16 +33,19 @@ class Item extends Component {
         return (
             <div className="videoThumb">
                 <GridTile
-                    onTouchTap={this.props.handleClick}
+                    //onTouchTap={this.props.handleClick}
                     title={this.props.video.title}
                     subtitle={< span > by < b > {
                     this.props.video.author
                 } </b></span >}
-                    actionIcon={<IconButton onTouchTap = {
-                    () => {
-                        this._handlePlay(this.props.video.assetid);
-                    }
-                } > <PlayCircleOutline color="white"/> </IconButton>}>
+                    actionIcon={<div>
+                        <IconButton iconStyle={styles.mediumIcon}  style={styles.medium} onTouchTap = {() => {this.props.handleClick();}} >
+                            <Info hoverColor={amber300} color={fullWhite}/>
+                        </IconButton>
+                        <IconButton iconStyle={styles.mediumIcon}  style={styles.medium} onTouchTap = {() => {this._handlePlay(this.props.video.assetid);}} >
+                            <PlayCircleOutline hoverColor={amber300} color={fullWhite}/>
+                        </IconButton>
+                    </div>}>
                     <img src={this.props.video.thumbnail}/>
                 </GridTile>
             </div>
