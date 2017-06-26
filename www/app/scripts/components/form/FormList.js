b9bdeafdce5b51c6c7102ada72cf81cba76dbe9f
Daniel Levings <dal@futureuniverse.com>
2017-06-26 11:37:16 +0200

    LIVE-46 Added more settings

---
+++ www/app/scripts/components/form/FormList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const FormList = () => (
+    <div>
+        <ul className="formList">
+            <li>Ronjas iPhone</li>
+            <li>Tonys Macbook</li>
+        </ul>
+    </div>
+);
+
+export default FormList;
--- www/app/scripts/containers/pages/SettingsPage.js
+++ www/app/scripts/containers/pages/SettingsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {RaisedButton, SelectField, MenuItem, Toggle, Snackbar} from 'material-ui';
+import {RaisedButton, SelectField, MenuItem, Toggle, Snackbar, TextField} from 'material-ui';
 import {changeLang, changeAudioLang, changeSubtitleLang, toggleRecommendations, saveUserSettings} from '../../actions/settings';
+import FormList from '../../components/form/FormList';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -8,10 +9,21 @@ const styles = {
   headline: {
     fontSize: 24,
     fontWeight: 400,
-    height: '100%'
+    height: '100%',
+    borderBottom: '1px solid rgba(0, 0, 0, .1)',
+    paddingBottom: '16px',
+    marginBottom: '8px'
   },
   container: {
-    padding: '20px'
+    padding: '20px',
+    paddingBottom: '60px'
+  },
+  underlinedHeaderInline: {
+      display: 'inline-block',
+      borderBottom: '1px solid rgba(0, 0, 0, .1)',
+      paddingBottom: '16px',
+      marginBottom: '16px',
+      marginTop: '32px'
   }
 };
 
@@ -24,7 +36,9 @@ class SettingsPage extends React.Component {
         super();
 
         this.state = {
-            snackbarIsOpen: false
+            snackbarIsOpen: false,
+            ageRating: "4",
+            parentalCode: "1234"
         };
 
     }
@@ -32,6 +46,7 @@ class SettingsPage extends React.Component {
     handleAudioChange = (event, index, value) => this.props.dispatch(changeAudioLang(value));
     handleSubtitleChange = (event, index, value) => this.props.dispatch(changeSubtitleLang(value));
     handleRecommendationsChange = () => this.props.dispatch(toggleRecommendations());
+    handleAgeRatingChange = (event, index, value) => this.setState({ageRating: value})
 
     handleSaveTouch = () => {
         this.props.dispatch(saveUserSettings(this.props.settings.options));
@@ -54,12 +69,14 @@ class SettingsPage extends React.Component {
         }
     }
 
+    
+
     render(){
         return(
           <div className="settings-page" style={styles.container}>
             <h1 style={styles.headline}>{i18next.t('setting_title')}</h1>
 
-            <h3>{i18next.t('setting_language_options')}</h3>
+            <h3 style={styles.underlinedHeaderInline}>{i18next.t('setting_language_options')}</h3>
 
             <SelectField
               style={selectBoxStyle}
@@ -92,12 +109,39 @@ class SettingsPage extends React.Component {
                 <MenuItem value={"nb"} primaryText={i18next.t('language_norwegian')} />
             </SelectField>
 
-            <h3>General Settings</h3>
+            <h3 style={styles.underlinedHeaderInline}>General Settings</h3>
             <Toggle
                 label="Reccomendations" 
                 toggled={this.props.settings.options.recommendations}
                 onToggle={this.handleRecommendationsChange}/>
                 <br />
+
+            <h3 style={styles.underlinedHeaderInline}>Active Devices</h3>
+            <FormList />
+            <br />
+
+            <h3 style={styles.underlinedHeaderInline}>Parental Controls</h3>
+
+            <SelectField
+                style={selectBoxStyle}
+                floatingLabelText="Age Rating"
+                value={this.state.ageRating}
+                onChange={this.handleAgeRatingChange} >
+                    <MenuItem value={"4"} primaryText="4+" />
+                    <MenuItem value={"9"} primaryText="9+" />
+                    <MenuItem value={"12"} primaryText="12+" />
+                    <MenuItem value={"17"} primaryText="17+" />
+                    <MenuItem value={"all"} primaryText="Allow All" />
+            </SelectField>
+
+            <TextField
+                hintText="####"
+                floatingLabelText="Parental PIN code"
+                type="password"
+                value={this.state.parentalCode}
+                />
+            <br />
+
             <RaisedButton label="Save" primary={true} onTouchTap={this.handleSaveTouch} />
 
             <Snackbar
