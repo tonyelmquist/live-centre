3b107785e44d348c986b2067cbad8a86b9f8efc8
Akmal Fayziyev <afs@futureuniverse.com>
2017-06-27 14:57:29 +0200

    LIVE-36 Updated product hover for a new video data structure

--- www/app/scripts/components/common/VideoDescription.js
+++ www/app/scripts/components/common/VideoDescription.js
@@ -5,8 +5,8 @@ import BorderStar from 'material-ui/svg-icons/toggle/star-border';
 import HalfStar from 'material-ui/svg-icons/toggle/star-half';
 import {yellow500} from 'material-ui/styles/colors';
 
-const VideoDescription = (props) =>  {
-    const info = props.video.data;
+const VideoDescription = ({video}) =>  {
+    // const info = props.video.data;
     const max_stars = 5;
     const _renderStars = () => {
         const stars = new Array(5);
@@ -29,12 +29,13 @@ const VideoDescription = (props) =>  {
             {/* {
                 props.title.length > 32 ? <h2>{props.title}</h2> : <h3>{props.title}</h3>
             } */}
-            <h1>{info.title}</h1>
+            <h1>{video._title}</h1>
             <h3>Episode Title</h3>
-            <span>Duration: {Math.round(info.length/60)} min</span>
+            <span>Duration: {Math.round(45)} min</span>
             <div>{_renderStars()}</div>
             <h3>Description</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>{video._description}</p>
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> */}
         </div>
     );
 
