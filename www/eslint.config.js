efe6da60d62f763c70444b8e607fe89c4597dc07
Akmal Fayziyev <afs@futureuniverse.com>
2017-06-28 13:04:09 +0200

    LIVE-4 Added prettier package, updated eslint config.

--- www/eslint.config.js
+++ www/eslint.config.js
@@ -1,36 +1,34 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true,
-            "arrowFunctions": true,
-            "blockBindings": true,
-            "generators": true,
-            "modules": true,
-            "experimentalObjectRestSpread": true
-        }
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            arrowFunctions: true,
+            blockBindings: true,
+            generators: true,
+            modules: true,
+            experimentalObjectRestSpread: true,
+        },
     },
     rules: {
-        "arrow-parens": 2,
-        "curly": 2,
-        "no-var": 2,
-        "prefer-arrow-callback": 2,
-        "prefer-const": 2,
-        "prefer-spread": 2,
-        "prefer-template": 2,
-        "require-yield": 2,
-        semi: ["error", "always"],
-        "react/prop-types": 1
+        'arrow-parens': 2,
+        curly: 2,
+        'no-var': 2,
+        'prefer-arrow-callback': 2,
+        'prefer-const': 2,
+        'prefer-spread': 2,
+        'prefer-template': 2,
+        'require-yield': 2,
+        semi: ['error', 'always'],
+        'react/prop-types': 1,
     },
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true,
-        "jasmine": true
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        jasmine: true,
     },
-    "plugins": [
-        "react"
-    ]
+    plugins: ['react'],
 };
