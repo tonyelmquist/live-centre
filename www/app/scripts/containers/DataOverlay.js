c533ee511e42ee138e2d5db43b79f2f7640f2235
Daniel Levings <dal@futureuniverse.com>
2017-06-29 15:45:20 +0200

    LIVE-51 Moved button into better place

--- www/app/scripts/containers/DataOverlay.js
+++ www/app/scripts/containers/DataOverlay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import BurstButton from '../components/BurstButton';
 
 const styles = {
     overlayStyle: {
@@ -17,16 +18,62 @@ const styles = {
         zIndex: 2147483647,
         marginLeft: 5,
         color: 'white',
-    },
+    },  
+    burstButtonContainer: {
+      position: 'absolute',
+      right: '0',
+      bottom: '-240px',
+      zIndex: 100000,
+      overflow: 'auto',
+  }
+
 };
 
 class DataOverlay extends Component {
 
+
+
     render() {
+
+            this.burstButtonLinks = [
+        {
+            action: function() {
+                console.log('Pressed ID 1');
+            },
+            icon: 'a'
+        },
+        {
+            action: function() {
+                console.log('Pressed ID 2');
+            },
+            icon: 'b'
+        },
+        {
+            action: function() {
+                console.log('Pressed ID 3');
+            },
+            icon: 'c'
+        },
+        {
+            action: function() {
+                console.log('Pressed ID 4');
+            },
+            icon: 'c'
+        },
+        {
+            action: function() {
+                console.log('Pressed ID 5');
+            },
+            icon: 'c'
+        },
+    ]
+
         return (
           <div style={styles.overlayStyle}>
             <div className="trapezoid" />
             <div style={styles.dataStyle}>Score: {this.props.score}</div>
+            <BurstButton buttonLinks={this.burstButtonLinks} style={styles.burstButtonContainer}/>
+
           </div>
         );
     }
