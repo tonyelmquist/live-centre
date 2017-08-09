9a709b0d08c24bb2605f50f5299157d285af3bba
Ronja Knudtsen <ronjaknudtsen@Ronjas-MacBook-Pro.local>
2017-08-09 17:37:09 +0200

    LIVE-143 Better images are now added to the channels page. And filtering should now work.

--- www/app/scripts/containers/pages/ChannelsPage.jsx
+++ www/app/scripts/containers/pages/ChannelsPage.jsx
@@ -7,60 +7,139 @@ import CircleRowItem from '../../components/HorizontalScroll/CircleRowItem';
 import MasonryContainer from '../../components/Masonry/MasonryContainer';
 import MasonryImageTile from '../../components/Masonry/MasonryImageTile';
 
-// Dummy filters and channels
-const filters = [
-  { key: 'Children', avatarStyle: {backgroundColor: '#E91E63'} },
-  { key: 'Drama', avatarStyle: {backgroundColor: '#FFC107'}},
-  { key: 'Pets', avatarStyle: {backgroundColor: '#00BCD4'}},
-  { key: 'Humor', avatarStyle: {backgroundColor: '#673AB7'}},
-  { key: 'Fantasy', avatarStyle: {backgroundColor: '#FF9800'}},
-  { key: 'Documentaries', avatarStyle: {backgroundColor: '#8BC34A'}},
-];
+import max from '../../../img/mockup/channels/max.png';
+import maxLOGO from '../../../img/mockup/channels/logo_max.png';
+import fem from '../../../img/mockup/channels/fem.png';
+import femLOGO from '../../../img/mockup/channels/logo_fem.png';
+
+import tlc from '../../../img/mockup/channels/tlc.png';
+// import tlcLOGO from '../../../img/mockup/channels/tlc.png';
+import tvNorge from '../../../img/mockup/channels/tvNorge.png';
+import tvNorgeLOGO from '../../../img/mockup/channels/logo_tvNorge.png';
+import vox from '../../../img/mockup/channels/vox.png';
+import animal from '../../../img/mockup/channels/animal.png';
+
+// // Dummy filters and channels
+// const filters = [
+//   { key: 'Children', avatarStyle: { backgroundColor: '#E91E63' } },
+//   { key: 'Drama', avatarStyle: { backgroundColor: '#FFC107' } },
+//   { key: 'Pets', avatarStyle: { backgroundColor: '#00BCD4' } },
+//   { key: 'Humor', avatarStyle: { backgroundColor: '#673AB7' } },
+//   { key: 'Fantasy', avatarStyle: { backgroundColor: '#FF9800' } },
+//   { key: 'Documentaries', avatarStyle: { backgroundColor: '#8BC34A' } },
+// ];
+
+// Keep for later if we want future group channel
+// const channels = {
+//     1: {
+//         id: 1,
+//         title: 'The Future Group',
+//         logo: '/img/demoChannels/lost-in-time-logo.png',
+//         thumbnail: '/img/demoChannels/lost-in-time.jpg',
+//     },
+//     2: {
+//         id: 2,
+//         title: 'Street Fighter',
+//         logo: '/img/demoChannels/street-fighter-logo.png',
+//         thumbnail: '/img/demoChannels/street-fighter.jpg',
+//     },
+// };
 
 const channels = {
     1: {
         id: 1,
-        title: 'The Future Group',
-        logo: '/img/demoChannels/lost-in-time-logo.png',
-        thumbnail: '/img/demoChannels/lost-in-time.jpg',
+        title: 'MAX',
+        logo: require('../../../img/mockup/channels/logo_max.png'),
+        thumbnail: require('../../../img/mockup/channels/max.png'),
+        tags: ['drama', 'humor', 'documentaries'],
     },
     2: {
         id: 2,
-        title: 'Street Fighter',
-        logo: '/img/demoChannels/street-fighter-logo.png',
-        thumbnail: '/img/demoChannels/street-fighter.jpg',
+        title: 'Fem',
+        logo: femLOGO,
+        thumbnail: fem,
+        tags: ['drama', 'humor'],
+    },
+    3: {
+        id: 3,
+        title: 'TLC',
+        // logo: tlcLOGO,
+        thumbnail: tlc,
+        tags: ['drama', 'humor'],
+    },
+    4: {
+        id: 4,
+        title: 'VOX',
+        // logo: vox,
+        thumbnail: vox,
+        tags: ['drama', 'humor'],
+    },
+    5: {
+        id: 5,
+        title: 'TV Norge',
+        logo: tvNorgeLOGO,
+        thumbnail: tvNorge,
+        tags: ['drama', 'humor', 'children', 'documentaries'],
+    },
+    6: {
+        id: 6,
+        title: 'Animal Planet',
+        thumbnail: animal,
+        tags: ['pets', 'animals', 'documentaries'],
     },
 };
 
 class ChannelsPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeFilter: -1,
+            filters: [
+              { key: 'children', avatarStyle: { backgroundColor: '#E91E63' } },
+              { key: 'drama', avatarStyle: { backgroundColor: '#FFC107' } },
+              { key: 'pets', avatarStyle: { backgroundColor: '#00BCD4' } },
+              { key: 'humor', avatarStyle: { backgroundColor: '#673AB7' } },
+              { key: 'fantasy', avatarStyle: { backgroundColor: '#FF9800' } },
+              { key: 'documentaries', avatarStyle: { backgroundColor: '#8BC34A' } },
+            ],
+        };
+    }
+
 
     changeRoute = path => this.props.history.push(path);
 
-    filterTile = () => {
-        console.log('filter');
+    handleFiltering = (index) => {
+        console.log('set state', index);
+        this.setState((prevState, props) => ({ activeFilter: index }));
     }
 
     handleOnClick = (item) => {
-        //console.log('Channel:', item);
+        // console.log('Channel:', item);
+        console.log(item.title);
         this.changeRoute(`/Channel/${item.title}`);
     }
 
     getTiles = () => {
         const tiles = [];
-        
+
+        console.log(this.state.activeFilter);
         for (const key in channels) {
-          tiles.push(
-        <MasonryImageTile
-          key={`channel-tile-${key}`}
-          poster={channels[key].thumbnail}
-          handleClick={() => this.handleOnClick(channels[key])}
-          overlay={
-              <img onTouchTap={() => this.handleOnClick(channels[key])} className="logo" src={channels[key].logo} />
+            if (this.state.activeFilter === -1 || channels[key].tags.indexOf(this.state.activeFilter) >= 0) {
+              console.log("TRUE", this.state.activeFilter);
+              console.log(channels[key].tags);
+                tiles.push(
+                  <MasonryImageTile
+                    key={`channel-tile-${key}`}
+                    poster={channels[key].thumbnail}
+                    handleClick={() => this.handleOnClick(channels[key])}
+                    overlay={
+                        <img onTouchTap={() => this.handleOnClick(channels[key])} className="logo" src={channels[key].logo} />
+                      }
+                  />,
+                );
             }
-        />,
-      );
-      }
-    
+        }
+
         return tiles;
     }
 
@@ -76,6 +155,7 @@ class ChannelsPage extends React.Component {
         overlay={(this.filterOverlay(filter))}
         key={filter.key}
         style={filter.avatarStyle}
+        handleClick={() => { this.handleFiltering(filter.key); }}
       />
     ))
 
@@ -88,7 +168,7 @@ class ChannelsPage extends React.Component {
 
         <div className="inline-circle-label">
           <HorizontalScrollContainer>
-            {this.getCircleFilters(filters)}
+            {this.getCircleFilters(this.state.filters)}
           </HorizontalScrollContainer>
         </div>
         <br />
@@ -99,7 +179,7 @@ class ChannelsPage extends React.Component {
           </div>
       </div>
 
-      );
+        );
     }
 }
 
--- www/app/scripts/containers/pages/SingleChannelPage.jsx
+++ www/app/scripts/containers/pages/SingleChannelPage.jsx
@@ -1,31 +1,27 @@
-import React, { component } from 'react';
+import React from 'react';
+import i18next from 'i18next';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MasonryContainer from '../../components/Masonry/MasonryContainer';
-import MasonryImageTile from '../../components/Masonry/MasonryImageTile';
-import MasonryTextOverlay from '../../components/Masonry/MasonryTextOverlay';
 import HorizontalScrollContainer from '../../components/HorizontalScroll/HorizontalScrollContainer';
 import CircleRowItem from '../../components/HorizontalScroll/CircleRowItem';
-import { videoSelected, resetCurrentTimeInPlayer } from '../../actions/videoPlayer';
-import { openOverlayX, maximizeOverlayX } from '../../actions/overlayX';
+import MasonryContainer from '../../components/Masonry/MasonryContainer';
+import MasonryImageTile from '../../components/Masonry/MasonryImageTile';
 
-// Dummy channels:
-const channels = {
-    1: {
-      id: 1,
-      title: 'Lost in Time',
-      logo: '/img/demoChannels/lost-in-time-logo.png',
-      thumbnail: '/img/demoChannels/lost-in-time.jpg',
-  },
-    2: {
-      id: 2,
-      title: 'Street Fighter',
-      logo: '/img/demoChannels/street-fighter-logo.png',
-      thumbnail: '/img/demoChannels/street-fighter.jpg',
-  },
-};
+import FilterTabs from '../../components/HorizontalScroll/FilterTabs';
 
-// Dummy filters
+import img1 from '../../../img/mockup/series/1.png';
+import img2 from '../../../img/mockup/series/2.png';
+import img3 from '../../../img/mockup/series/3.jpg';
+import img4 from '../../../img/mockup/series/4.png';
+import img5 from '../../../img/mockup/series/5.png';
+import img6 from '../../../img/mockup/series/movies/6.png';
+import img7 from '../../../img/mockup/series/movies/7.png';
+import img8 from '../../../img/mockup/series/movies/8.png';
+
+
+
+
+// Dummy filters and channels
 const filters = [
   { key: 'Children', avatarStyle: {backgroundColor: '#E91E63'} },
   { key: 'Drama', avatarStyle: {backgroundColor: '#FFC107'}},
@@ -35,106 +31,137 @@ const filters = [
   { key: 'Documentaries', avatarStyle: {backgroundColor: '#8BC34A'}},
 ];
 
+const programs = {
+    1: {
+        id: 1,
+        title: 'Big bang theory',
+        thumbnail: img1,
+        tags: ['series', 'drama', 'humor'],
+    },
+    2: {
+        id: 2,
+        title: 'Insider',
+        thumbnail: img2,
+        tags: ['series','drama',],
+    },
+    3: {
+        id: 3,
+        title: 'IT Crowd',
+        thumbnail: img3,
+        tags: ['series','drama', 'humor', 'documentaries'],
+    },
+    4: {
+        id: 4,
+        title: 'Special Victims Unit',
+        thumbnail: img4,
+        tags: ['series','drama', 'humor', 'documentaries'],
+    },
+    5: {
+        id: 5,
+        title: 'Flash',
+        thumbnail: img5,
+        tags: ['series','drama', 'humor', 'documentaries'],
+    },
+    6: {
+        id: 6,
+        title: 'Avatar',
+        thumbnail: img6,
+        tags: ['movies','drama', 'humor', 'documentaries'],
+    },
+    7: {
+        id: 7,
+        title: 'Nausicca',
+        thumbnail: img7,
+        tags: ['movies','drama', 'humor', 'documentaries'],
+    },
+    8: {
+        id: 8,
+        title: 'Wolverine',
+        thumbnail: img8,
+        tags: ['movies', 'drama', 'humor', 'documentaries'],
+    },
+};
 
-class SingleChannelPage extends React.Component {
-    //! ! Uses tags as channels since we have no channels per now.
-    getTiles = () => {
-        const channelKey = this.props.match.params.channelKey;
-        const tiles = [];
-        const channelVideos = this.props.tags[channelKey].videos;
-        const videos = this.props.videos.items;
+class ChannelsPage extends React.Component {
 
-        for (const key in channelVideos) {
-            const videoKey = channelVideos[key];
-            const video = videos[videoKey];
-
-            tiles.push(
-              <MasonryImageTile
-                key={`single-channel-video-${videoKey}`}
-                poster={video.thumbnail}
-                handleClick={() => this.handleTileOpen(video)}
-                overlay={<MasonryTextOverlay video={video} handleTilePlay={this.handleTilePlay} title={video.title} />}
-              />,
-            );
+    constructor(props){
+        super(props);
+        this.state = {
+            activeTab: 0,
+            tabs: ['all', 'series', 'movies', 'drama', 'comedy', 'documentaries'],
         }
+    }
 
+    filterTile = () => {
+        console.log('filter');
+    }
+
+    getTiles = () => {
+        const tiles = [];
+        
+        for (const key in programs) {
+            const activeTab = this.state.tabs[this.state.activeTab];
+            if (this.state.activeTab === 0 || programs[key].tags.indexOf(activeTab) >= 0) {
+                console.log("true", this.state.activeTab);
+                tiles.push(
+                    <MasonryImageTile
+                    key={`channel-tile-${key}`}
+                    poster={programs[key].thumbnail}
+                    handleClick={() => this.changeRoute('/Channel/Street Fighter')}
+                    //handleClick={() => this.handleOnClick(channels[key])}
+                    //overlay={<img onTouchTap={() => this.handleOnClick(channels[key])} className="logo" src={channels[key].logo} />}
+                    />,
+                );
+            }
+
+      }
+
+      console.log(tiles);
+    
         return tiles;
     }
 
-    getFilters = () => {
-        const rowItems = [];
-        for (const key in filters) {
-            rowItems.push(
-          <CircleRowItem
-              key={key}
-              handleClick={() => this.handleFilter(key)}
-              overlay={(this.filterOverlay(filters[key]))}
-              style={filters[key].avatarStyle}
-          />,
-        );
-        }
-        return rowItems;
+    changeTab = (index) => {
+        this.setState((prevState, props) => {
+            return {activeTab : index}
+        });
     }
-
-    handleTilePlay = (video) => {
-        this.props.dispatch(openOverlayX());
-        this.props.dispatch(maximizeOverlayX());
-        this.props.dispatch(videoSelected(video));
-        this.props.dispatch(resetCurrentTimeInPlayer());
-    }
-
-    handleTileOpen = (video) => {
-        this.props.dispatch(openOverlayX());
-        this.props.dispatch(maximizeOverlayX());
-        this.props.dispatch(videoSelected(video));
-        this.props.dispatch(resetCurrentTimeInPlayer());
-    }
-
-    handleFilter = () => {
-        console.log('handle filter of channels');
-    }
-
-    filterOverlay = filter => (
-      <div className="circleFilterOverlay">
-        {filter.key}
-      </div>
-    )
 
     render() {
         const channelKey = this.props.match.params.channelKey;
 
+        
         return (
-          <div>
-            <div className="container-fluid">
-              <h4>{i18next.t('filter')}</h4>
-            </div>
-            <HorizontalScrollContainer>
-              {this.getFilters()}
-            </HorizontalScrollContainer>
-            <br />
-            <div className="container-fluid">
-              <h4> LIVE Now </h4>
-            <MasonryContainer>
-                {this.props.videos.videosFetched > 0
-                  ? this.getTiles()
-                  : <div />
-                }
-            </MasonryContainer>
-            </div>
-          </div>
-        );
-    }
+        <div className="channel-page">
 
+        <FilterTabs
+            tabItems={this.state.tabs}
+            //activeTab={this.props.activetab}
+            changeTab={this.changeTab}
+            activeTab={this.state.activeTab}
+            colortheme="dark"
+        />
+
+        <br />
+        <div className="container-fluid">
+          <MasonryContainer>
+            {this.getTiles()}
+          </MasonryContainer>
+          </div>
+      </div>
+
+      );
+    }
 }
 
-SingleChannelPage.propTypes = {
+ChannelsPage.propTypes = {
     videos: PropTypes.any.isRequired,
     tags: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-    videos: state.videos,
+    videos: state.videos.items,
     tags: state.tags.items,
 });
 
-export default connect(mapStateToProps)(SingleChannelPage);
+export default connect(mapStateToProps)(ChannelsPage);
