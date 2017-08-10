8cbea60310cf82b9c1d410faa3ee3abe8b522b65
John Anton "Tony" Elmquist <tel@futureuniverse.com>
2017-08-10 11:18:07 +0200

    LIVE-108 added a couple more timelines

--- www/app/scripts/constants/timelines.js
+++ www/app/scripts/constants/timelines.js
@@ -6,36 +6,86 @@ const timelines = [
                 timestamp: 9332,
                 type: 'ecom',
                 productID: 1,
-                productDescription: 'A Boot!',
-                thumbnailImage: '/img/boot.png',
             },
             {
                 timestamp: 18043,
                 type: 'ecom',
                 productID: 2,                
-                productDescription: 'A Duck!',
-                thumbnailImage: '/img/duck.png',
             },
             {
                 timestamp: 33245,
                 type: 'ecom',
                 productID: 3,
-                productDescription: 'A Spider!',
-                thumbnailImage: '/img/spider.png',
             },
             {
                 timestamp: 41225,
                 type: 'ecom',
                 productID: 4,
-                productDescription: 'A Spatula!',
-                thumbnailImage: '/img/spatula.png',
             },
             {
                 timestamp: 50000,
                 type: 'ecom',
                 productID: 5,
-                productDescription: 'A Knife!',
-                thumbnailImage: '/img/knife.png',
+            },
+        ],
+    },
+    {
+        video: 222905001,
+        events: [
+            {
+                timestamp: 9332,
+                type: 'ecom',
+                productID: 3,
+            },
+            {
+                timestamp: 18043,
+                type: 'ecom',
+                productID: 1,                
+            },
+            {
+                timestamp: 33245,
+                type: 'ecom',
+                productID: 4,
+            },
+            {
+                timestamp: 41225,
+                type: 'ecom',
+                productID: 5,
+            },
+            {
+                timestamp: 50000,
+                type: 'ecom',
+                productID: 2,
+            },
+        ],
+    },
+    {
+        video: 222904001,
+        events: [
+            {
+                timestamp: 9332,
+                type: 'ecom',
+                productID: 2,
+            },
+            {
+                timestamp: 18043,
+                type: 'ecom',
+                productID: 5,
+            },
+            {
+                timestamp: 33245,
+                type: 'ecom',
+                productID: 1,
+            },
+            {
+                timestamp: 41225,
+                type: 'ecom',
+                productID: 3,
+            },
+            {
+                timestamp: 50000,
+                type: 'ecom',
+                productID: 4,
             },
         ],
     },
