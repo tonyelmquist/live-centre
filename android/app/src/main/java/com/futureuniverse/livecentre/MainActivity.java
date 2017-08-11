96fc2486c263dde8c762706e8e9e9283288ad057
John Anton "Tony" Elmquist <tel@futureuniverse.com>
2017-08-11 11:49:36 +0200

    Changed to vanilla webview, added web paths for images

--- android/app/src/main/java/com/futureuniverse/livecentre/MainActivity.java
+++ android/app/src/main/java/com/futureuniverse/livecentre/MainActivity.java
@@ -1,131 +1,42 @@
 package com.futureuniverse.livecentre;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
-import org.xwalk.core.JavascriptInterface;
-import org.xwalk.core.XWalkPreferences;
-import org.xwalk.core.XWalkView;
-
 import com.futureuniverse.livecentre.R;
 
-public class MainActivity extends FragmentActivity {
-    KeyboardAwareLinearLayout mainView;
-    XWalkView mXview;
-    JavascriptBridge mJavascriptBridge;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView mWebView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
-        setContentView(R.layout.activity_main);
-
-        mainView = (KeyboardAwareLinearLayout)findViewById(R.id.main);
-        mainView.setOnKeyboardChangedListener(new KeyboardAwareLinearLayout.OnKeyboardChangedListener() {
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        mWebView = new WebView(this);
+        mWebView.loadUrl("file:///android_asset/index.html");
+        mWebView.getSettings().setJavaScriptEnabled(true); 
+        mWebView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onClosed() {
-                int azlh = 0;
-            }
-
-            @Override
-            public void onOpened() {
-                int azlh = 0;
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
             }
         });
 
-        setXView();
-    }
-
-    private void setXView(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
-        }
-        XWalkPreferences.setValue(XWalkPreferences.ALLOW_UNIVERSAL_ACCESS_FROM_FILE, true);
-
-        mXview = (XWalkView)findViewById(R.id.x_walk_view);
-
-        mXview.addJavascriptInterface(this, "tfgNative");
-        mJavascriptBridge = new JavascriptBridge(this, mXview);
-
-        mXview.load("file:///android_asset/index.html", null);
-//        mXview.load("file:///android_asset/kitchen-sink-material/index.html", null);
-
-/*
-        VideoView videoview = (VideoView)findViewById(R.id.videoView);
-        videoview.setMediaController(new MediaController(this));
-        videoview.setVideoURI(Uri.parse("url://video"));
-        videoview.requestFocus();
-        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
-
-                int seekForwardTime = 59000;
-                if (mp != null) {
-                    int currentPosition = mp.getCurrentPosition();
-                    if (currentPosition + seekForwardTime <= mp.getDuration()) {
-                        mp.seekTo(currentPosition + seekForwardTime);
-                    } else {
-                        mp.seekTo(mp.getDuration());
-                    }
-                }
-
-
-                //mp.seekTo(59000);
-            }
-        });
-*/
-
+        this.setContentView(mWebView);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        mJavascriptBridge.onPause();
-        if (mXview != null) {
-            mXview.pauseTimers();
-            mXview.onHide();
+    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
         }
+        return super.onKeyDown(keyCode, event);
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mJavascriptBridge.onResume();
-        if (mXview != null) {
-            mXview.resumeTimers();
-            mXview.onShow();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mXview != null) {
-            mXview.onDestroy();
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        mJavascriptBridge.onBackPressed();
-        //super.onBackPressed();
-    }
-
-    @JavascriptInterface
-    public boolean jsBridge(final String requestString) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                final String urlPrefix = "tfg://";
-                if (requestString.startsWith(urlPrefix)) {
-                    mJavascriptBridge.callNative(requestString.substring(urlPrefix.length()));
-                }
-            }
-        });
-        return true;
-    }
-
 }
+
--- www/app/scripts/constants/products.js
+++ www/app/scripts/constants/products.js
@@ -4,7 +4,7 @@ const productArray =
             {
                 productID: 1,
                 productTitle: 'A Boot!',
-                thumbnailImage: '/img/boot.png',
+                thumbnailImage: 'http://www.seamuffin.com/images/boot.png',
                 description:
           'This is a wonderful boot. It boots like very few boots can boot. If you are in the market for a boot that can really boot, this boot is the boot for you.',
                 price: 12.99,
@@ -12,7 +12,7 @@ const productArray =
             {
                 productID: 2,
                 productTitle: 'A duck!',
-                thumbnailImage: '/img/duck.png',
+                thumbnailImage: 'http://www.seamuffin.com/images/duck.png',
                 description:
           'This is a wonderful duck. It ducks like very few ducks can duck. If you are in the market for a duck that can really duck, this duck is the duck for you.',
                 price: 12.99,
@@ -20,7 +20,7 @@ const productArray =
             {
                 productID: 3,
                 productTitle: 'A spider!',
-                thumbnailImage: '/img/spider.png',
+                thumbnailImage: 'http://www.seamuffin.com/images/spider.png',
                 description:
           'This is a wonderful spider. It spiders like very few spiders can spider. If you are in the market for a spider that can really spider, this spider is the spider for you.',
                 price: 12.99,
@@ -28,7 +28,7 @@ const productArray =
             {
                 productID: 4,
                 productTitle: 'A spatula!',
-                thumbnailImage: '/img/spatula.png',
+                thumbnailImage: 'http://www.seamuffin.com/images/spatula.png',
                 description:
           'This is a wonderful spatula. It spatulas like very few spatulas can spatula. If you are in the market for a spatula that can really spatula, this spatula is the spatula for you.',
                 price: 12.99,
@@ -36,7 +36,7 @@ const productArray =
             {
                 productID: 5,
                 productTitle: 'A knife!',
-                thumbnailImage: '/img/knife.png',
+                thumbnailImage: 'http://www.seamuffin.com/images/knife.png',
                 description:
           'This is a wonderful knife. It knifes like very few knifes can knife. If you are in the market for a knife that can really knife, this knife is the knife for you.',
                 price: 12.99,
