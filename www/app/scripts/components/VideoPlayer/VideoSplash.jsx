49efa4590fae48bf340f5eaab6aa49b9459dfb5e
Ronja Knudtsen <ronjaknudtsen@Ronjas-MacBook-Pro.local>
2017-09-05 16:41:37 +0200

    LIVE-381 Added last styles of tickers and commenting. Motion did not work as there was not enough control. Replaced with own animation. Now only need to hook it up with the actual data.

--- www/app/scripts/components/VideoPlayer/VideoSplash.jsx
+++ www/app/scripts/components/VideoPlayer/VideoSplash.jsx
@@ -14,42 +14,44 @@ class VideoSplash extends React.Component {
     }
     componentDidMount() {
         this.setEnterStyle();
-        setTimeout(() => { this.fadeOutNewItem() ;}, 4000);
+        const timeBeforeFadeout = 4000;
+        setTimeout(() => { this.fadeOutNewItem(); }, timeBeforeFadeout);
     }
 
+    // When a new item enters fade and slide it in.
     setEnterStyle = () => {
         this.setState({ style: { x: 0, o: 1, y: 100 } });
     }
-
+    // Then fade it out again.
     fadeOutNewItem = () => {
-        this.setState((prevState) => ({ style: { ...prevState.style, o: 0 } }));
+        this.setState(prevState => ({ style: { ...prevState.style, o: 0 } }));
         // this.setState({ style.o: 0 });
     }
 
 
     getStyle = (x, o, y) =>
-        // console.log(x, o, y);
          ({
              transform: `translateY(${x}px) skew(-10deg)`,
-             height: y,
+             height: y, // Use height so the old items dont jump.
              opacity: o,
          });
 
     getOpacity = () => {
         const fadeExpo = 0.3;
-        if (this.props.showHistory){
-            if(this.props.currentSplash === this.props.id){
-                return 1
-            } 
-                return 1 - ((this.props.currentSplash - this.props.id) * fadeExpo)
-            
-        } else {
-            if(this.props.currentSplash === this.props.id){
-                return this.state.style.o;
-            } else {
-                return 0
+        // If we are showing the items:
+        if (this.props.showHistory) {
+            // The focused splash should be fully visible
+            if (this.props.currentSplash === this.props.id) {
+                return 1;
             }
+            // The rest should fade depending on the distance from the items.
+            return 1 - ((this.props.currentSplash - this.props.id) * fadeExpo);
         }
+        // IF not showing items and this is the splash in focus.
+        if (this.props.currentSplash === this.props.id) {
+            return this.state.style.o; // Use the state opacity (fade in on insert).
+        }
+        return 0; // Hide all other.
     }
 
     render() {
--- www/app/scripts/components/VideoPlayer/VideoSplashContainer.jsx
+++ www/app/scripts/components/VideoPlayer/VideoSplashContainer.jsx
@@ -3,24 +3,51 @@ import PropTypes from 'prop-types';
 import VideoSplash from './VideoSplash';
 
 import { Motion, spring, TransitionMotion } from 'react-motion';
+import Scroll from 'react-scroll'; // Imports all Mixins
 
 class VideoSplashContainer extends React.Component {
     constructor(props) {
         super(props);
 
+        //Two states, either you are actively scrolling, or you are not, and then the scroller will scroll smoth to bottom. 
         this.state = {
             messages: [
                 { message: 'Free kick Real Madrid' },
             ],
-            currentSplash: 0,
-            scrollTop: 0,
+            currentSplash: 0, //The newest splash message is always current splash. 
+            splashInFocus: 0, //The splash that is in focus (The bottom splash, or whatever we have scrolled to).
+            scrollTo: 0, // Use this when the user is not scrolling and we want to reset to bottom.
+            isScrolling: false, // If we are auto scrolling, then we dont need to check whish splash is in focus. 
+            preventAutoScroll: false, // Should we prevent auto scroll. The user is actively scrolling.
+            showHistory: false, //Should we show the history, passed down to props to change the opacity of the items. 
         };
     }
 
     // Simulates messages.
     componentDidMount() {
-        // setTimeout(() => { this.pushSplash("hest"); }, 6000);
         this.pushAllMessages();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('splashInFocus:', this.state.splashInFocus);
+        // IF preventautoscroll, wait til until it is Not preventautoscroll
+            // then if new item, scroll down, 
+            // if we have hidden the items, scroll down (reset).
+        if (!this.state.preventAutoScroll) {
+            if (this.state.currentSplash !== this.state.splashInFocus) {
+                console.log('scroll To Bottom', this.state.currentSplash);
+                this.scrollToBottom();
+            }
+            // } else {
+            //     console.log("Splash is same", this.state);
+            // }
+            // else if (!this.state.showHistory && (this.state.showHistory !== prevState.showHistory)) {
+            //     this.scrollToBottom();
+            //     //console.log('scrollToBottom');
+            // } else {
+            //     //console.log('dont scroll to bottom');
+            // }
+        } else { console.log('prevent autoscroll'); }
     }
 
     pushAllMessages = () => {
@@ -37,103 +64,109 @@ class VideoSplashContainer extends React.Component {
             { message: "Felix Brych signals a throw-in for Juventus Turin, close to Juventus Turin's area." },
         ];
         for (const key in newMessages) {
-            setTimeout(() => { this.pushSplash(newMessages[key].message); }, 8000 * key);
+            setTimeout(() => { this.pushSplash(newMessages[key].message); }, 4000 * key);
         }
     }
 
+    // Push the new message
     pushSplash = (message) => {
         this.setState(prevState => ({
             messages: [...prevState.messages, { key: 4, message }],
             currentSplash: prevState.messages.length,
         }));
-
-        this.scrollToBottom();
     }
 
+    // Smooth animating scrollbar with help from the internet. 
+    easeOutCubic = (currentIteration, startValue, changeInValue, totalIterations) => changeInValue * (Math.pow(currentIteration / totalIterations - 1, 3) + 1) + startValue;
+
+    moveScroller = (from, to, currentIteration) => {
+        const totalIterations = 600; // Max duration.
+        const MPS = 10;
+        const changeInValue = to - from;
+        const newPos = this.easeOutCubic(currentIteration, from, changeInValue, totalIterations);
+        if (currentIteration >= totalIterations) {
+            this.scroller.scrollTop = newPos;
+            this.setState({isScrolling: false});
+            return newPos;
+        }
+        this.scroller.scrollTop = newPos;
+
+        setTimeout(() => {
+            this.moveScroller(from, to, currentIteration + MPS);
+        }, MPS);
+    }
+
+    animateScrollBarFromTo = (from, to) => {
+        const startIteration = 1;
+        this.moveScroller(from, to, startIteration);
+    }
+
+    // Scroll to bottom of page
     scrollToBottom = () => {
-        const scroller = this.splashScroll.scroller;
         const padding = 20 * 2;
-        // console.log(scroller.scrollHeight, scroller.clientHeight);
+        const scroller = this.scroller;
+        //Set the new item (currentsplash) as the focus. 
+        if (this.state.splashInFocus !== this.state.currentSplash) {
+            this.setState({ splashInFocus: this.state.currentSplash, isScrolling: true });
+        }
+        //Check if we need to scroll (if the items height exceed the height of the window)
+        //Wait 500 milisec so we know the new item has been put into the scrollbar to prevent jumping.
         if ((window.innerHeight + padding) <= scroller.clientHeight) {
-            //Set timeout because we need to wait until the height of the new item is adjusted.
             setTimeout(() => {
-                console.log('scroll from ', scroller.scrollTop, 'to', scroller.scrollHeight - scroller.offsetHeight);
-                this.setState({ scrollTop: scroller.scrollHeight - scroller.offsetHeight }); 
-            }, 300);
-        } else {
-            return false;
+                //Scroll from where the scroller is now to the full height (bottom) of the page. 
+                const scrollFrom = this.scroller.scrollTop;
+                const scrollTo = this.scroller.scrollHeight - this.scroller.offsetHeight;
+                // this.changeScroller(scrollFrom, scrollTo);
+                console.log('scroll from ', scrollFrom, 'to ', scrollTo);
+                this.animateScrollBarFromTo(scrollFrom, scrollTo);
+            }, 500);
         }
     }
 
-
-    render() {
-        const { messages, currentSplash } = this.state;
-
-        return (
-            <Motion style={{ scrollTop: spring(this.state.scrollTop) }}>
-                {motion => <SplashScroll ref={ref => (this.splashScroll = ref)} scrollTop={motion.scrollTop} messages={messages} currentSplash={currentSplash} />}
-            </Motion>
-        );
-    }
-}
-
-class SplashScroll extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            showHistory: false,
-            preventAutoScroll: false,
-        };
-    }
-
+    //Stop propagation to not pause the video. 
     onTouchStart = (e) => {
         e.stopPropagation();
-        console.log("touch start")
-        if (!this.state.preventAutoScroll) {
-            this.setState({ preventAutoScroll: true });
-            this.showHistory();
-        }
+        this.showHistory();
+        clearTimeout(this._hideScrollerTimeout); 
     }
+    // User has released touch, wait X sec and scroll down + fade out items.
     onTouchEnd = (e) => {
         e.stopPropagation();
-        console.log("touchEnd, preventAutoscroll = false");
-        if (this.state.preventAutoScroll) {
-            setTimeout(() => {
-                this.setState({ preventAutoScroll: false });
-            }, 5000);
-        }
+
+        this._hideScrollerTimeout = setTimeout(() => {
+            this.setState({ preventAutoScroll: false });
+            this.setState({ showHistory: false });
+        }, 3000);  
+    }
+    onTouchTap = (e) => {
+        e.stopPropagation();
+        //clearTimeout(this._hideScrollerTimeout); 
     }
 
     onScroll = (e) => {
         e.stopPropagation();
+        this.checkScrollPos();
     }
 
-    showHistory = () => {
-        console.log("showHistory");
-        this.setState({ showHistory: true });
-        const time = 8000;
-
-        setTimeout(() => {
-            if (!this.state.preventAutoScroll) {
-                console.log("hide history");
-                this.setState({ showHistory: false });
-                // Scroll to top.
+    checkScrollPos = (e) => {
+        if (!this.state.isScrolling){ //If we are autoscrolling, we dont need to check
+            const padding = 20 + 20;
+            const bottom = this.scroller.scrollHeight - this.scroller.offsetHeight;
+            if (((bottom - this.scroller.scrollTop) / 110) > 1){ //Each item is 110px high. 
+                //Divide this by the amount scrolled and we know how many items to shif the focis. 
+                this.setState({splashInFocus: this.state.currentSplash + (this.scroller.scrollTop - bottom) / 110});
             }
-        }, time);
-    }
-
-    //Executed on render 
-    scrollToBottom = () => {
-        
-        if (this.scroller.scrollTop != this.props.scrollTop && !this.state.preventAutoScroll) {
-            console.log("Scroll to bottom");
-            this.scroller.scrollTop = this.props.scrollTop;
         }
     }
 
+    //Show splash history 
+    showHistory = () => {
+        if (!this.state.preventAutoScroll) { this.setState({ preventAutoScroll: true }); }
+        this.setState({ showHistory: true });
+    }
+
     render() {
-        const { messages, currentSplash } = this.props;
+        const { messages, currentSplash } = this.state;
         const style = {
             position: 'absolute',
             right: 0,
@@ -141,13 +174,8 @@ class SplashScroll extends React.Component {
             padding: '20px',
             width: '40%',
             maxHeight: '100%',
-            // height: '100%',
             overflow: 'scroll',
         };
-
-        if (this.scroller != undefined) {
-            this.scrollToBottom();
-        }
 
         return (
             <div
@@ -159,18 +187,17 @@ class SplashScroll extends React.Component {
                 onTouchEnd={this.onTouchEnd}
             >
                 {messages.map((message, key) => (
-                       <VideoSplash
+                    <VideoSplash
                             showHistory={this.state.showHistory}
                             preventAutoScroll={this.state.preventAutoScroll}
                             message={message.message}
-                            currentSplash={currentSplash}
+                            currentSplash={this.state.splashInFocus}
                             id={key}
-                       />
+                    />
                     ))}
             </div>
         );
     }
 }
 
-// //ref={ref => (this.scroller = ref)} onScroll={this.handleScroll.bind(this)}
 export default VideoSplashContainer;
