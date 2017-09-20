db3c6a8019b5e40a51447e1625559ba0c569e39f
Daniel Levings <dal@futureuniverse.com>
2017-09-20 16:19:04 +0200

    LIVE-423 Increased padding to hard to click areas on

--- www/app/scripts/components/SecondLayer/LineupOverlay.jsx
+++ www/app/scripts/components/SecondLayer/LineupOverlay.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import LineupShirt from './LineupShirt';
+
 import LineupShirtManager from './LineupShirtManager';
 
 class LineupOverlay extends Component {
--- www/app/scripts/components/SecondLayer/PlayerInfoOverlay.jsx
+++ www/app/scripts/components/SecondLayer/PlayerInfoOverlay.jsx
@@ -31,9 +31,8 @@ class PlayerInfoOverlay extends Component {
         console.log(role);
         if (role.length > 0) {
             return role[0].team.name + ' #' + role[0].jersey_number;
-        } else {
-            return 'Not Active';
-        }
+        } 
+        return 'Not Active';
     }
 
     render() {
@@ -64,22 +63,22 @@ class PlayerInfoOverlay extends Component {
                 </span>
               <div className="player-info-tabs">
                 <div className="player-info-tab tab-player-info" onClick={this.state.isModalOpen ? this.closeModal : this.openModal} role="button" tabIndex="0"><i className="fa fa-pie-chart" /> Statistics</div>
-                {/*<div className="player-info-tab tab-statistics" onClick={this.closeModal} role="button" tabIndex="0"><i className="fa fa-bar-chart" /></div>*/}
-                {/*<div className="player-info-tab tab-shopping"><i className="fa fa-shopping-cart" /></div>*/}
+                {/* <div className="player-info-tab tab-statistics" onClick={this.closeModal} role="button" tabIndex="0"><i className="fa fa-bar-chart" /></div>*/}
+                {/* <div className="player-info-tab tab-shopping"><i className="fa fa-shopping-cart" /></div>*/}
               </div>
             </div>
 
             {teamMember.statistics ?
                 (<div className="player-modal">
-                    {/*<div className="player-stats player-assists">{teamMember.statistics.assists}</div>*/}
+                    {/* <div className="player-stats player-assists">{teamMember.statistics.assists}</div>*/}
                     <div className="player-stats player-matches_played">Games: {teamMember.statistics.matches_played}</div>
                     <div className="player-stats player-goals_scored">Goals: {teamMember.statistics.goals_scored}</div>
-                    {/*<div className="player-stats player-own_goals">{teamMember.statistics.own_goals}</div>*/}
+                    {/* <div className="player-stats player-own_goals">{teamMember.statistics.own_goals}</div>*/}
                     <div className="player-stats player-red_cards">{teamMember.statistics.red_cards} Red Cards</div>
-                    {/*<div className="player-stats player-substituted_in">{teamMember.statistics.substituted_in}</div>*/}
-                    {/*<div className="player-stats player-substituted_out">{teamMember.statistics.substituted_out}</div>*/}
+                    {/* <div className="player-stats player-substituted_in">{teamMember.statistics.substituted_in}</div>*/}
+                    {/* <div className="player-stats player-substituted_out">{teamMember.statistics.substituted_out}</div>*/}
                     <div className="player-stats player-yellow_cards">{teamMember.statistics.yellow_cards} Yellow Cards</div>
-                    {/*<div className="player-stats player-yellow_red_cards">{teamMember.statistics.yellow_red_cards}</div>*/}
+                    {/* <div className="player-stats player-yellow_red_cards">{teamMember.statistics.yellow_red_cards}</div>*/}
                 </div>)
             : <div />}
 
