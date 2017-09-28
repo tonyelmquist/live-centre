46fe525efb181a2d2950ef03b2a21ea8d34b5d04
Daniel Levings <dal@futureuniverse.com>
2017-09-28 11:18:10 +0200

    LIVE-441 Removed jitter on screen rotation

--- www/app/scripts/containers/App.jsx
+++ www/app/scripts/containers/App.jsx
@@ -65,25 +65,23 @@ class App extends Component {
     }
 
     dispatchOrientation = () => {
-        setTimeout(() => {
-            if (typeof window.orientation !== 'undefined') {
-                if (window.orientation === 0) {
-                    this.props.dispatch(setPortrait());
-                }
-
-                if (window.orientation === -90 || window.orientation === 90) {
-                    this.props.dispatch(setLandscape());
-                }
-            } else {
-                if (window.innerHeight > window.innerWidth) {
-                    this.props.dispatch(setPortrait());
-                }
-
-                if (window.innerHeight < window.innerWidth) {
-                    this.props.dispatch(setLandscape());
-                }
+        if (typeof window.orientation !== 'undefined') {
+            if (window.orientation === 0) {
+                this.props.dispatch(setPortrait());
             }
-        }, 10);
+
+            if (window.orientation === -90 || window.orientation === 90) {
+                this.props.dispatch(setLandscape());
+            }
+        } else {
+            if (window.innerHeight > window.innerWidth) {
+                this.props.dispatch(setPortrait());
+            }
+
+            if (window.innerHeight < window.innerWidth) {
+                this.props.dispatch(setLandscape());
+            }
+        }
     }
 
     closeTeamMemberOverlay = () => {
